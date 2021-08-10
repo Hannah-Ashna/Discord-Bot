efa6c8a8836fe654ab32cf08f437ed9f66fe826a
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2021-08-10 22:07:48 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 bot = discord.Client()
-watchParty = discord.Embed(title = "Watch Party List:")
+
 
 async def stay_awake():
     await bot.wait_until_ready()
@@ -20,7 +20,8 @@ async def stay_awake():
         channel = bot.get_channel(874759682839937024)
         
         if (current_time > start_time and current_time < end_time):
-            watchParty.add_field(name = "Who is joining us?", value = "NO ONE", inline = False)
+            watchParty = discord.Embed(title = "Watch Party List:")
+            watchParty.add_field(name = "List is currently empty", value = "Do /Me to join", inline = False)
             await channel.send(embed = watchParty)
         
         else:
@@ -38,19 +39,14 @@ async def on_message(message):
     # Greet the user and show them their tasks for the day
     if (message.content.startswith("Morning") or message.content.startswith("morning") or message.content.startswith("mrnin") 
         or message.content.startswith("mornin") or message.content.startswith("Mornin") or message.content.startswith("G'day") or message.content.startswith("g'day")):
-        
-        await message.channel.send("Hello nerd")
+        UserName = (str(message.author)).split("#")
+        await message.channel.send("Hello " + UserName[1])
         
     # Call them out for sleeping too much! 
     if (message.content.startswith("afternoon")):
         await message.channel.send("Damn ... took you long enough...")
 
-    if (message.content.startswith(".me")):
-        UserName = (str(message.author)).split("#")
-        watchParty.add_field(name = "Who is joining us?", value = UserName[1], inline = False)
 
-    if (message.content.startswith(".partylist")):
-        await message.channel.send(embed = watchParty)
 
 async def on_ready():
     print('Logged in as: ',bot.user.name)

0e55cd9f0c6095d28b8ef7645f2f59e2dd758eb3
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2021-08-10 21:19:15 +0100

    test 2

--- bot.py
+++ bot.py
@@ -7,6 +7,25 @@ from datetime import datetime
 TOKEN = os.getenv('DISCORD_TOKEN')
 bot = discord.Client()
 
+async def stay_awake():
+    await bot.wait_until_ready()
+    while not bot.is_closed():
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+
+        if (current_time == "21:20:00"):
+            watchParty = discord.Embed(title = "Watch Party List:")
+            watchParty.add_field(name = "aaaa", value = "aaaaa", inline = False)
+            channel = bot.get_channel('<#874748237616140338>')
+            await channel.send(embed = watchParty)
+        
+        else:
+            channel = bot.get_channel('<#874748237616140338>')
+            await channel.send("uuuh")
+        print('Im awake :)')
+        await asyncio.sleep(1680) #runs every 28mins.
+
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
@@ -15,25 +34,15 @@ async def on_message(message):
     # Greet the user and show them their tasks for the day
     if (message.content.startswith("Morning") or message.content.startswith("morning") or message.content.startswith("mrnin") 
         or message.content.startswith("mornin") or message.content.startswith("Mornin") or message.content.startswith("G'day") or message.content.startswith("g'day")):
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        await message.channel.send("Hello Human ;)" + current_time)
+        
+        await message.channel.send("Hello H0M0")
         
     # Call them out for sleeping too much! 
     if (message.content.startswith("afternoon")):
-        await message.channel.send("Damn ... took you long enough... time to look at your tasks!")
-
-    # Provide list of commands using .help
-    if (message.content.startswith(".help")):
-        helpCommand = discord.Embed(title = "Commands List:")
-        helpCommand.add_field(name = ".SetTask", value = "Make a to-do list using \n.SetTask, Task1, Task2, ...", inline = False)
-        helpCommand.add_field(name = ".AddTask", value = "Add items to the to-do list \n.AddTask, Task1, Task2, ...", inline = False)
-        helpCommand.add_field(name = ".DeleteTask", value = "Remove items from the to-do list \n.DeleteTask, Task1, Task2, ...", inline = False)
-        helpCommand.add_field(name = ".Bands", value = "This displays the band names list", inline = False)
-        helpCommand.add_field(name = ".AddBand", value = "Add a band name to the list \n.AddBand, Task1, Task2, ...", inline = False)
-        await message.channel.send(embed = helpCommand)
+        await message.channel.send("Damn ... took you long enough...")
 
 async def on_ready():
     print('Logged in as: ',bot.user.name)
-    
+
+bot.loop.create_task(stay_awake())
 bot.run(TOKEN)

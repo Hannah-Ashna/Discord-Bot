b40ad8c4709d4ce15758e0f7cc7358b2eaa996c4
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2021-08-19 20:23:59 +0100

    Removed old embed + fixed lowercase commands

--- bot.py
+++ bot.py
@@ -5,9 +5,11 @@ import asyncio
 from datetime import datetime
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL = os.getenv('CHANNEL_ID')
+
 bot = discord.Client()
-watchParty = discord.Embed(title = "Watch Party List:")
 watchList = []
+
 async def stay_awake():
     await bot.wait_until_ready()
     while not bot.is_closed():
@@ -17,19 +19,15 @@ async def stay_awake():
         start_time = "13:00:00"
         end_time = "14:00:00"
 
-        channel = bot.get_channel(874759682839937024)
+        channel = bot.get_channel(CHANNEL)
         
         # Setup the watchParty list
         if (current_time > start_time and current_time < end_time):
-            await channel.send("Current watch party list is empty!\nDo .Me to join...")
+            await channel.send("Current watch party list is empty!\nDo .Join to join...")
         
         # Reset the watchParty list for the next day
         elif (current_time < start_time):
-            watchParty = []
-            watchParty = discord.Embed(title = "Watch Party List:")
-
-        else:
-            await channel.send(embed = watchParty)
+            watchList = []
 
         print('Im awake!')
         await asyncio.sleep(1680) #runs every 28mins
@@ -41,17 +39,16 @@ async def on_message(message):
         return
 
     # Greet the user and show them their tasks for the day
-    if (message.content.startswith("Morning") or message.content.startswith("morning") or message.content.startswith("mrnin") 
-        or message.content.startswith("mornin") or message.content.startswith("Mornin") or message.content.startswith("G'day") or message.content.startswith("g'day")):
+    if (message.content.lower("morning") or message.content.lower("mrnin") or message.content.lower("mornin") or message.content.lower("g'day")):
         UserName = (str(message.author)).split("#")
         await message.channel.send("**Hello** " + UserName[0])
         
     # Call them out for sleeping too much! 
-    if (message.content.startswith.lower("afternoon")):
+    if (message.content.lower("afternoon")):
         await message.channel.send("Damn ... took you long enough...")
 
     # Add a server member to the watch party list
-    if (message.content.startswith.lower(".Me")):
+    if (message.content.lower(".join")):
         UserName = (str(message.author)).split("#")
         if UserName[0] not in watchList:
             watchList.append(UserName[0])
@@ -60,7 +57,7 @@ async def on_message(message):
             await message.channel.send("You're already on the list! Use `.Bail` to leave.")
 
     # Remove a server member from the watch party list
-    if (message.content.startswith.lower(".Bail")):
+    if (message.content.lower(".bail")):
         UserName = (str(message.author)).split("#")
         if UserName[0] in watchList:
             watchList.remove(UserName[0])
@@ -69,13 +66,11 @@ async def on_message(message):
             await message.channel.send("You're not on the list! Use `.Me` to join.")
 
     # Display current watch party list attendees
-    if (message.content.startswith(".Partylist")):
+    if (message.content.lower(".partylist")):
         usersList = "**Joining us Tonight:**\n"
         for x in range (0, len(watchList)):
             usersList += "- " + watchList[x] + "\n"
         await message.channel.send(usersList)
-
-
 
 async def on_ready():
     print('Logged in as: ',bot.user.name)

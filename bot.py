c4826e9f8ef9bcb33e056db267c072b4b9fc1354
Hannah Ashna Jacob <hannah.ashna.jacob@gmail.com>
2021-10-04 19:31:06 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ async def stay_awake():
         channel = bot.get_channel(CHANNEL)
         
         # Setup the watchParty list
-        if (now.hour > 19 and now.hour < 20):
+        if (now.hour >= 19 and now.hour <= 20):
             await channel.send("Current watch party list is empty!\nDo .Join to join...")
 
         print('Im awake!')

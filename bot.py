f8c87c885fcd21b3455bf18025a47c38cebc7047
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2022-01-08 14:46:22 +0000

    Structural Rework p.1
    Moving away from the on message structure and instead using the in-built command-prefix functionality

--- bot.py
+++ bot.py
@@ -3,15 +3,23 @@ import discord
 import os
 import asyncio
 import random
-from datetime import datetime
 from imdb import IMDb
+from datetime import datetime
+from discord.ext import commands
 
+# Environment Variables
 TOKEN = os.getenv('DISCORD_TOKEN')
 CHANNEL = os.getenv('CHANNEL_ID')
 
-bot = discord.Client()
+# Other Variables
 watchList = []
 
+# Discord Bot Setup Stuff
+bot = commands.Bot(command_prefix="!")
+
+# This is to keep the bot from falling asleep while hosted on Heroku.
+# Obviously this can be removed once we move to a private host
+# that won't time us out all the time ... 
 async def stay_awake():
     await bot.wait_until_ready()
     while not bot.is_closed():
@@ -25,6 +33,48 @@ async def stay_awake():
 
         await asyncio.sleep(1680) #runs every 28mins
 
+
+# Command: Figure out if Jad is busy or not
+@bot.command(
+    name = "jad",
+    brief = "Use this to figure out if Jad is available or not."
+)
+async def jad_availability(ctx):
+    now = datetime.now()
+    if (now.hour > 9 and now.hour < 17 and (now.weekday() != 5 and now.weekday() != 6)):
+        await ctx.channel.send("He's probably at work but might still respond.")
+    elif (now.hour > 9 and now.hour < 23):
+        await ctx.channel.send("I mean he's probably free?")
+    else:
+        await ctx.channel.send("Jad's dead. F.")
+
+# Command: Join the nightly watch party VIP list
+@bot.command(
+    name = "join",
+    brief = "Use this to join the watch party VIP list!"
+)
+async def join_watch_party(ctx):
+    if (ctx.content.lower().startswith(".join")):
+        UserName = (str(ctx.author)).split("#")
+        if UserName[0] not in watchList:
+            watchList.append(UserName[0])
+            await ctx.channel.send("... Adding you to the **VIP** list")
+        else:
+            await ctx.channel.send("You're already on the list! Use `.Bail` to leave.")
+
+# Command: Leave the nightly watch party VIP list
+@bot.command(
+    name = "bail",
+    brief = "Use this to leave the watch party VIP list :("
+)
+async def leave_watch_party(ctx):
+    if (ctx.content.lower().startswith(".bail")):
+        UserName = (str(ctx.author)).split("#")
+        if UserName[0] in watchList:
+            watchList.remove(UserName[0])
+            await ctx.channel.send("... damn, the **audacity**")
+        else:
+            await ctx.channel.send("You're not on the list! Use `.Me` to join.")
 
 @bot.event
 async def on_message(message):
@@ -42,24 +92,6 @@ async def on_message(message):
     if (message.content.lower().startswith("afternoon")):
         await message.channel.send("Damn ... took you long enough...")
 
-    # Add a server member to the watch party list
-    if (message.content.lower().startswith(".join")):
-        UserName = (str(message.author)).split("#")
-        if UserName[0] not in watchList:
-            watchList.append(UserName[0])
-            await message.channel.send("... Adding you to the **VIP** list")
-        else:
-            await message.channel.send("You're already on the list! Use `.Bail` to leave.")
-
-    # Remove a server member from the watch party list
-    if (message.content.lower().startswith(".bail")):
-        UserName = (str(message.author)).split("#")
-        if UserName[0] in watchList:
-            watchList.remove(UserName[0])
-            await message.channel.send("... damn, the **audacity**")
-        else:
-            await message.channel.send("You're not on the list! Use `.Me` to join.")
-
     # Display current watch party list attendees
     if (message.content.lower().startswith(".partylist")):
         usersList = "**Joining us Tonight:**\n"
@@ -70,17 +102,6 @@ async def on_message(message):
     # Danny easter egg
     if (message.content.lower().startswith("danny")):
        await message.channel.send("hee hoo")
-
-    # Jad busy command
-    if (message.content.lower().startswith(".jadbusy?")):
-        now = datetime.now()
-        
-        if (now.hour > 9 and now.hour < 17 and (now.weekday() != 5 and now.weekday() != 6)):
-            await message.channel.send("He's probably at work but might still respond.")
-        elif (now.hour > 9 and now.hour < 23):
-            await message.channel.send("I mean he's probably free?")
-        else:
-            await message.channel.send("Jad's dead. F.")
 
     # Movie Suggestions
     if (message.content.lower().startswith(".findmovie")):
